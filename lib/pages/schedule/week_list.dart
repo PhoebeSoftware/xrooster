9be12166470110168a2ee3cfcecb8609be745818
Alex Jonker <alex.jonker2008@gmail.com>
2025-10-13 23:32:52 +0200

    Week scrolling

--- lib/pages/schedule/week_list.dart
+++ lib/pages/schedule/week_list.dart
@@ -15,12 +15,20 @@ class WeekListState extends State<WeekList> {
   late String selectedDayString;
   PageController? _pageController;
   VoidCallback? _pageListener;
+  late PageController _weekPageController;
 
   @override
   void initState() {
     super.initState();
     selectedDayString = DateFormat('yyyy-MM-dd').format(DateTime.now());
+    _weekPageController = PageController(initialPage: 1000);
     WidgetsBinding.instance.addPostFrameCallback((_) => _attachController());
+  }
+
+  @override
+  void dispose() {
+    _weekPageController.dispose();
+    super.dispose();
   }
 
   @override
@@ -58,10 +66,46 @@ class WeekListState extends State<WeekList> {
           setState(() {
             selectedDayString = dayString;
           });
+          _scrollToSelectedDay();
         }
       };
 
       _pageController!.addListener(_pageListener!);
+    }
+  }
+
+  void _scrollToSelectedDay() {
+    final selectedDate = DateFormat('yyyy-MM-dd').parse(selectedDayString);
+
+    final selectedWeekday = selectedDate.weekday;
+    final daysFromMonday = selectedWeekday == DateTime.sunday
+        ? 6 // Zondag is 6 dagen na maandag van de vorige week
+        : selectedWeekday - DateTime.monday;
+
+    final selectedWeekMonday = selectedDate.subtract(
+      Duration(days: daysFromMonday),
+    );
+
+    final now = DateTime.now();
+    final nowWeekday = now.weekday;
+    final currentWeekDaysFromMonday = nowWeekday == DateTime.sunday
+        ? 6
+        : nowWeekday - DateTime.monday;
+
+    final currentWeekMonday = now.subtract(
+      Duration(days: currentWeekDaysFromMonday),
+    );
+
+    final weeksDifference =
+        selectedWeekMonday.difference(currentWeekMonday).inDays ~/ 7;
+    final targetPage = 1000 + weeksDifference;
+
+    if (_weekPageController.hasClients) {
+      _weekPageController.animateToPage(
+        targetPage,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
     }
   }
 
@@ -71,76 +115,86 @@ class WeekListState extends State<WeekList> {
     final now = DateTime.now();
     final currentDay = DateFormat('yyyy-MM-dd').format(now);
 
-    final screenWidth = MediaQuery.of(context).size.width;
     const itemCount = 7;
+    final screenWidth = MediaQuery.of(context).size.width;
     final buttonWidth = screenWidth / itemCount;
 
     return Material(
       color: theme.colorScheme.surface,
       child: SizedBox(
         height: 70,
-        child: ListView.builder(
-          physics: const NeverScrollableScrollPhysics(),
-          scrollDirection: Axis.horizontal,
-          itemCount: itemCount,
-          padding: EdgeInsets.zero,
-          itemBuilder: (context, index) {
-            final monday = now.subtract(
+        child: PageView.builder(
+          controller: _weekPageController,
+          itemCount: null, // Infinite scrolling
+          itemBuilder: (context, weekIndex) {
+            final baseMonday = now.subtract(
               Duration(days: now.weekday - DateTime.monday),
             );
-            final day = monday.add(Duration(days: index));
-            final dayString = DateFormat('yyyy-MM-dd').format(day);
-            final isToday = dayString == currentDay;
-            final isSelected = dayString == selectedDayString;
+            final weekOffset = weekIndex - 1000;
+            final weekMonday = baseMonday.add(Duration(days: weekOffset * 7));
 
             return SizedBox(
-              width: buttonWidth,
-              child: OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                  backgroundColor: isToday
-                      ? theme.colorScheme.onPrimary
-                      : theme.cardColor,
-                  side: BorderSide(
-                    color: theme.colorScheme.primary,
-                    width: 3,
-                    style: isSelected ? BorderStyle.solid : BorderStyle.none,
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  minimumSize: const Size(70, 70),
-                  padding: EdgeInsets.zero,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      DateFormat.E('nl').format(day),
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 21.0,
-                        color: isSelected || isToday
-                            ? theme.colorScheme.primary
-                            : theme.colorScheme.onSurface,
+              width: screenWidth,
+              child: Row(
+                children: List.generate(itemCount, (dayIndex) {
+                  final day = weekMonday.add(Duration(days: dayIndex));
+                  final dayString = DateFormat('yyyy-MM-dd').format(day);
+                  final isToday = dayString == currentDay;
+                  final isSelected = dayString == selectedDayString;
+
+                  return SizedBox(
+                    width: buttonWidth,
+                    child: OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        backgroundColor: isToday
+                            ? theme.colorScheme.onPrimary
+                            : theme.cardColor,
+                        side: BorderSide(
+                          color: theme.colorScheme.primary,
+                          width: 3,
+                          style: isSelected
+                              ? BorderStyle.solid
+                              : BorderStyle.none,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        minimumSize: const Size(70, 70),
+                        padding: EdgeInsets.zero,
                       ),
-                    ),
-                    const SizedBox(height: 4),
-                    Text(
-                      DateFormat('d MMM', 'nl').format(day),
-                      style: TextStyle(
-                        fontWeight: FontWeight.normal,
-                        fontSize: 14.0,
-                        color: isSelected || isToday
-                            ? theme.colorScheme.primary
-                            : theme.colorScheme.onSurface,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            DateFormat.E('nl').format(day),
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 21.0,
+                              color: isSelected || isToday
+                                  ? theme.colorScheme.primary
+                                  : theme.colorScheme.onSurface,
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            DateFormat('d MMM', 'nl').format(day),
+                            style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 14.0,
+                              color: isSelected || isToday
+                                  ? theme.colorScheme.primary
+                                  : theme.colorScheme.onSurface,
+                            ),
+                          ),
+                        ],
                       ),
+                      onPressed: () {
+                        setState(() => selectedDayString = dayString);
+                        widget.rooster.currentState?.changeDate(dayString);
+                      },
                     ),
-                  ],
-                ),
-                onPressed: () {
-                  setState(() => selectedDayString = dayString);
-                  widget.rooster.currentState?.changeDate(dayString);
-                },
+                  );
+                }),
               ),
             );
           },
