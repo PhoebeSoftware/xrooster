4ec679fbe1f22be31ab9280ad2a2cfcf8128e99e
Alex Jonker <alex.jonker2008@gmail.com>
2026-08-24 12:53:12 +0200

    Fix inappwebview on some linux systems

--- linux/runner/main.cc
+++ linux/runner/main.cc
@@ -1,6 +1,8 @@
 #include "my_application.h"
 
 int main(int argc, char** argv) {
+  setenv("LIBGL_ALWAYS_SOFTWARE", "1", 1);
+  setenv("FLUTTER_INAPPWEBVIEW_LINUX_DISABLE_GL", "1", 1);
   g_autoptr(MyApplication) app = my_application_new();
   return g_application_run(G_APPLICATION(app), argc, argv);
 }
